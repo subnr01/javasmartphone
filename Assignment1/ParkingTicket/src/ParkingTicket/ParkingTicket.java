2a9c5d579727845a331fa91e0252d797aa9e0e08
subnr01 <subnr01@gmail.com>
2015-09-05 11:41:03 -0400

    Cointoss 1.0

---
+++ Assignment1/ParkingTicket/src/ParkingTicket/ParkingTicket.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ParkingTicket;
+
+/**
+ * @author admin
+ *
+ */
+public class ParkingTicket {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
